e4ab840d04023cbf79cf8c9ee922b3bff5bc4770
samriddhi7307 <72978417+samriddhi7307@users.noreply.github.com>
2020-11-27 19:57:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+var ball;
+var position,database;
+
+function setup(){
+    createCanvas(500,500);
+    database = firebase.database();
+    ball = createSprite(250,250,10,10);
+    ballposition = database.ref('ball/position');
+    ballposition.on('value',readPosition,showError);
+    ball.shapeColor = "red";
+}
+
+function draw(){
+    background("white");
+    if(position !== undifined){}
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function writePosition(x,y){
+    database.ref('ball/position').set({
+  'x': position.x+x,
+  'y' : position.y+y
+    })
+}
+function readPosition(data){
+    position = data.val();
+    ball.x = position.x;
+    ball.y = position.y;
+}
+
+function showError(){
+    console.log("this is an error");
+}
